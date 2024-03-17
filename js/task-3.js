84593f52d56a8bb454284c9b92a12c53624350fd
Iryna Serhiienko <ivus0888@gmail.com>
2024-03-17 19:59:46 +0200

    hw-08

--- js/task-3.js
+++
@@ -1,9 +0,0 @@
-const nameInput = document.getElementById('name-input');
-const nameOutput = document.getElementById('name-output');
-
-nameInput.addEventListener('input', () => {
-  const inputValue = nameInput.value.trim();
-  const outputName = inputValue === '' ? 'Anonymous' : inputValue;
-
-  nameOutput.textContent = outputName;
-});
